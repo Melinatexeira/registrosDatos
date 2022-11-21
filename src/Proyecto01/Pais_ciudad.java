65dfa6bd87afe52361ef6ecb539010cdc87b72bc
Melinatexeira <117683416+Melinatexeira@users.noreply.github.com>
2022-11-21 18:24:00 -0300

    Update Pais_ciudad.java

--- src/Proyecto01/Pais_ciudad.java
+++ src/Proyecto01/Pais_ciudad.java
@@ -1,2 +1,17 @@
-package Proyecto01;public class Pais_ciudad {
+package Proyecto01;
+
+public class Pais_ciudad {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        String pais, capital;
+
+        System.out.println("Ingrese Pais:");
+        pais = teclado.nextLine();
+        System.out.println("Ingrese Capital:");
+        capital = teclado.nextLine();
+
+        System.out.println("El Pais" + " " + pais + "," + "tiene como capital, la ciudad de" + " " + capital);
+    }
+}
 }
