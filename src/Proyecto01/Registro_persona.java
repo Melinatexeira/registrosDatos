40a458ce5ff83a94adea16cd18ddaccdfcc38229
Melinatexeira <117683416+Melinatexeira@users.noreply.github.com>
2022-11-21 18:25:46 -0300

    Update Registro_persona.java

--- src/Proyecto01/Registro_persona.java
+++ src/Proyecto01/Registro_persona.java
@@ -1,2 +1,16 @@
-package Proyecto01;public class Registro_persona {
+package Proyecto01;
+
+public class Registro_persona {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        String fname, lname;
+
+        System.out.println("Ingrese un nombre:");
+        fname = teclado.nextLine();
+        System.out.println("Ingrese un apellido:");
+        lname = teclado.nextLine();
+
+        System.out.println("Bienvenido"+ " " + fname + " " + lname +  " " + "al sistema...");
+    }
 }
