56eaf50126a258dbb62924d9eeb16f9606b8a793
Melinatexeira <117683416+Melinatexeira@users.noreply.github.com>
2022-11-21 18:29:22 -0300

    Update Registro_persona1.java

--- src/Proyecto01/Registro_persona1.java
+++ src/Proyecto01/Registro_persona1.java
@@ -1,2 +1,16 @@
-package Proyecto01;public class Registro_persona1 {
+package Proyecto01;
+public class Registro_persona1 {
+  
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese una edad:");
+        int age = teclado.nextInt();
+        System.out.println("Ingrese una estatura:");
+        int height = teclado.nextInt();
+
+        System.out.println("Su edad es:" + " " + age + " " + " y, su estatura:" + " "+ height);
+
+    }
 }
